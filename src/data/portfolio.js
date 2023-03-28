805bd4b44c8cdb70aec48f8a4ed64e436aa4c85d
clint911 <clingyking007@gmail.com>
2023-03-28 12:36:31 +0300

    rectifying images

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,19 +1,19 @@
 export default [
   {
   title: 'Taarifu',
-  imgUrl: '/public/assets/Taarifu.png',
+  imgUrl: '../../public/static/Taarifu.png',
   stack: ['Solidity', 'Reactjs', 'Typescript'],
   link: 'Taarifu.vercel.app',
   },
   {
   title: 'Cypto Devs',
-  imgUrl: '/public/assets/crypto-devs.png',
+  imgUrl: '../../public/static/crypto-devs.png',
   stack: ['Solidity', 'Nextjs', 'Typescript'],
   link: 'https://crypto-devs-nft-three.vercel.app/',
   },
   {
   title: 'E-PlantShop',
-  imgUrl: '/public/assets/plantshop.png',
+  imgUrl: '../../public/static/plantshop.png',
   stack: ['Solidity', 'Reactjs', 'Typescript'],
   link: 'https://e-commerce-plantshop.vercel.app/',
   }
