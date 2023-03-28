bcac989a6f3b034a370d5716bdf2fe0e66742bc0
clint911 <clingyking007@gmail.com>
2023-03-28 13:46:44 +0300

    I can't beleive I am fucking fixing this bs again.Vercel kindly make your platform more beginner friendly

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,19 +1,19 @@
 export default [
   {
   title: 'Taarifu',
-  imgUrl: '/public/images/Taarifu.png',
+  imgUrl: '/assets/Taarifu.png',
   stack: ['Solidity', 'Reactjs', 'Typescript'],
   link: 'Taarifu.vercel.app',
   },
   {
   title: 'Cypto Devs',
-  imgUrl: '/public/images/crypto-devs.png',
+  imgUrl: '/assets/crypto-devs.png',
   stack: ['Solidity', 'Nextjs', 'Typescript'],
   link: 'https://crypto-devs-nft-three.vercel.app/',
   },
   {
   title: 'E-PlantShop',
-  imgUrl: '/public/images/plantshop.png',
+  imgUrl: '/assets/plantshop.png',
   stack: ['Solidity', 'Reactjs', 'Typescript'],
   link: 'https://e-commerce-plantshop.vercel.app/',
   }
